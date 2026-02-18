1e68b016f4b381c5545cc0391a0fa96bab0c2b49
Petro <petrotymchuck@gmail.com>
2026-02-18 09:08:15 +0200

    add expense category entity

---
+++ WebMonyAPI/WebMonyAPI/Data/AppDbContext.cs
@@ -0,0 +1,5 @@
+﻿namespace WebMonyAPI.Data;
+
+public class AppDbContext
+{
+}
---
+++ WebMonyAPI/WebMonyAPI/Entities/Categories/ExpenseCategoryEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebMonyAPI.Entities.Base;
+
+namespace WebMonyAPI.Entities.Categories;
+
+[Table("tbl_expense_category")]
+public class ExpenseCategoryEntity : BaseEntity<long>
+{
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+}
