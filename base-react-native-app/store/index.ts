ae9db76f03fee31e64b63f7aaf5275c3df3d1dad
Petro <petrotymchuck@gmail.com>
2026-02-22 09:00:20 +0200

    create base query rtk

---
+++ base-react-native-app/store/index.ts
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {type TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import {setupListeners} from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+    reducer: {
+
+    },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(
+
+        )
+});
+
+setupListeners(store.dispatch);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
