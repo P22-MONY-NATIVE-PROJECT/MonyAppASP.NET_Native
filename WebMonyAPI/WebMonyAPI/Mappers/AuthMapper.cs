8fea2ad5b95fc47eab270d4e79b591712bd4964b
Petro <petrotymchuck@gmail.com>
2026-03-01 07:32:26 +0200

    add seeder logic

---
+++ WebMonyAPI/WebMonyAPI/Mappers/AuthMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebMonyAPI.Entities.Identity;
+using WebMonyAPI.SeedData.Models;
+
+namespace WebMonyAPI.Mappers;
+
+public class AuthMapper : Profile
+{
+    public AuthMapper()
+    {
+        CreateMap<SeederUserModel, UserEntity>()
+                .ForMember(x => x.UserName, opt => opt.MapFrom(x => x.Email));
+    }
+}
