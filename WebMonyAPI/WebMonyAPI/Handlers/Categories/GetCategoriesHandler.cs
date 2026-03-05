a359580dde65f65462579163752a25f8049aa48d
Petro <petrotymchuck@gmail.com>
2026-03-05 16:49:05 +0200

    add user logic to category

--- WebMonyAPI/WebMonyAPI/Handlers/Categories/GetCategoriesHandler.cs
+++ WebMonyAPI/WebMonyAPI/Handlers/Categories/GetCategoriesHandler.cs
@@ -8,15 +8,19 @@ using WebMonyAPI.Queries.Categories;
 
 namespace WebMonyAPI.Handlers.Categories;
 
-public class GetCategoriesHandler(IGenericRepository<CategoryEntity, long> repo, IMapper mapper)
+public class GetCategoriesHandler(IGenericRepository<CategoryEntity, long> repo, 
+    IMapper mapper,
+    IIdentityService identityService)
     : IRequestHandler<GetCategoriesQuery, IEnumerable<CategoryDto>>
 {
     public async Task<IEnumerable<CategoryDto>> Handle(
         GetCategoriesQuery request,
         CancellationToken cancellationToken)
     {
+        long userId = await identityService.GetUserIdAsync();
+
         var entities = await repo.AsQurable()
-            .Where(x=>x.CategoryTypeId==request.typeId)
+            .Where(x=>x.CategoryTypeId==request.typeId && x.UserId == userId)
             .ToListAsync();
         return mapper.Map<IEnumerable<CategoryDto>>(entities);
     }
