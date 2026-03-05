c75e6733229a7167949b0fb4054e5b8d9b57a0e7
Petro <petrotymchuck@gmail.com>
2026-03-05 06:25:49 +0200

    add exception catch

--- WebMonyAPI/WebMonyAPI/Controllers/BalancesController.cs
+++ WebMonyAPI/WebMonyAPI/Controllers/BalancesController.cs
@@ -1,6 +1,5 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
 using WebMonyAPI.Commands.Finances;
 using WebMonyAPI.Dtos.Finances;
 using WebMonyAPI.Queries.Finances;
@@ -15,32 +14,77 @@ public class BalancesController(IMediator mediator) : ControllerBase
     [HttpGet]
     [Authorize]
     public async Task<IActionResult> GetAll()
-        => Ok(await mediator.Send(new GetAllBalancesQuery()));
+    {
+        try
+        {
+            var result = await mediator.Send(new GetAllBalancesQuery());
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
 
     [HttpGet("{id:long}")]
     [Authorize]
     public async Task<IActionResult> GetById(long id)
-        => Ok(await mediator.Send(new GetBalanceByIdQuery(id)));
+    {
+        try
+        {
+            var result = await mediator.Send(new GetBalanceByIdQuery(id));
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
+    }
 
     [HttpPost]
     [Consumes("multipart/form-data")]
     [Authorize]
-    public async Task<IActionResult> Create(
-        [FromForm] CreateBalanceDto dto)
-        => Ok(await mediator.Send(new CreateBalanceCommand(dto)));
+    public async Task<IActionResult> Create([FromForm] CreateBalanceDto dto)
+    {
+        try
+        {
+            var result = await mediator.Send(new CreateBalanceCommand(dto));
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
 
     [HttpPut]
     [Consumes("multipart/form-data")]
     [Authorize]
-    public async Task<IActionResult> Update(
-        [FromForm] UpdateBalanceDto dto)
-        => Ok(await mediator.Send(new UpdateBalanceCommand(dto)));
+    public async Task<IActionResult> Update([FromForm] UpdateBalanceDto dto)
+    {
+        try
+        {
+            var result = await mediator.Send(new UpdateBalanceCommand(dto));
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
 
     [HttpDelete("{id:long}")]
     [Authorize]
     public async Task<IActionResult> Delete(long id)
     {
-        await mediator.Send(new DeleteBalanceCommand(id));
-        return NoContent();
+        try
+        {
+            await mediator.Send(new DeleteBalanceCommand(id));
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
     }
 }
