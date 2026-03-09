07af3c7706aadd647c3645997661dff667082dc7
Petro <petrotymchuck@gmail.com>
2026-03-09 06:23:56 +0200

    add all methods to service

--- base-react-native-app/services/operationsService.ts
+++ base-react-native-app/services/operationsService.ts
@@ -1,9 +1,10 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
-import { serialize } from "object-to-formdata";
 
 import { createBaseQuery } from "@/utilities/createBaseQuery";
 import {IOperationItemResponse} from "@/types/operation/IOperationItemResponse";
 import {ICreateOperationRequest} from "@/types/operation/ICreateOperationRequest";
+import {IDeleteOperationRequest} from "@/types/operation/IDeleteOperationRequest";
+import {IGetCategoryByIdRequest} from "@/types/category/IGetCategoryByIdRequest";
 
 export const operationsService = createApi({
     reducerPath: "api/operations",
@@ -25,6 +26,30 @@ export const operationsService = createApi({
                 body: body //serialize(body)
             }),
             invalidatesTags: ["Operations","Operation"]
+        }),
+
+        editOperation: builder.mutation<void, IEditOperationRequest>({
+            query: body => ({
+                url: "",
+                method: "PUT",
+                body: body
+            }),
+            invalidatesTags: ["Operations","Operation"]
+        }),
+
+        deleteOperation: builder.mutation<void, IDeleteOperationRequest>({
+            query: params => ({
+                url: `/${params.id}`,
+                method: "DELETE"
+            }),
+            invalidatesTags: ["Operations"]
+        }),
+
+        getOperationById: builder.query<IOperationItemResponse, IGetCategoryByIdRequest>({
+            query: params => ({
+                url: `/${params.id}`,
+            }),
+            providesTags: ["Operation"]
         }),
     }),
 });
