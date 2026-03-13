a4d3c2a58e714a835b14ec8c4882dd7b2bbfc837
Petro <petrotymchuck@gmail.com>
2026-03-13 10:20:42 +0200

    add dtos for reset password

---
+++ WebMonyAPI/WebMonyAPI/Dtos/Auth/AccountChangePasswordDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebMonyAPI.Dtos.Auth;
+
+public class AccountChangePasswordDto
+{
+    public string OldPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+}
---
+++ WebMonyAPI/WebMonyAPI/Dtos/Auth/AccountValidateResetTokenDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebMonyAPI.Dtos.Auth;
+
+public class AccountValidateResetTokenDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+}
