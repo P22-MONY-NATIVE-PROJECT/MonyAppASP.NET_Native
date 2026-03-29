7d1582618cfe8f80bc165dc16ef9b464946cb636
Petro <petrotymchuck@gmail.com>
2026-03-29 07:26:03 +0300

    add base notification

--- notification_test_android_app/app/src/main/java/com/example/notification/MainActivity.kt
+++ notification_test_android_app/app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,47 +1,107 @@
 package com.example.notification
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.notification.ui.theme.NotificationTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val CHANNEL_ID = "simple_notification_channel"
+    private val NOTIFICATION_ID = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Створюю канал сповіщень
+        createNotificationChannel()
+
+        // Створюю кнопку
         setContent {
             NotificationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(onClick = {
+                            showNotification()
+                        }) {
+                            Text(text = "Натисни для сповіщення!")
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    // Канали сповіщень потрібні для групування сповіщень в системі
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Основні сповіщення"
+            val descriptionText = "Канал для звичайних сповіщень додатка"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotificationTheme {
-        Greeting("Android")
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+                setShowBadge(true) // Це дозволяє показувати "крапку/бейдж" над іконкою
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification() {
+        // Налаштовую саме сповіщення
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("Нове повідомлення!")
+            .setContentText("Це твоє перше сповіщення.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setNumber(1)
+            .setAutoCancel(true) // Видаляти сповіщення, коли користувач на нього клікає
+
+        with(NotificationManagerCompat.from(this)) {
+            // Перевірка дозволу
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ActivityCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                        101
+                    )
+                    return
+                }
+            }
+            // Показуємо сповіщення
+            notify(NOTIFICATION_ID, builder.build())
+        }
     }
 }
